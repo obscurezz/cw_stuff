9411ca5c31bf274c8f0d7b13a8b8da5e63e0ef00
Andrey Kopylov <104315220+obscurezz@users.noreply.github.com>
2022-06-21 20:40:20 +0300

    new task executed

--- triangles.py
+++ triangles.py
@@ -1,1 +1,21 @@
+def triangle(row):
+    color_dict = {
+        ('G', 'G'): 'G',
+        ('R', 'R'): 'R',
+        ('B', 'B'): 'B',
+        ('B', 'G'): 'R',
+        ('R', 'G'): 'B',
+        ('B', 'R'): 'G',
+    }
+    new_row = ''
+    if len(row) == 1:
+        return row
+    else:
+        for i in range(len(row) - 1):
+            for k, v in color_dict.items():
+                if sorted((row[i], row[i + 1])) == sorted(k):
+                    new_row += v
+        return triangle(new_row)
 
+
+print(triangle('RRGBRGBB'))
